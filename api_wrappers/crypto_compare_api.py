393db768e1aaa7977a9ae1c6e91b58714a105534
Daniel Rodriguez Mariblanca <dakixr@gmail.com>
2021-10-21 20:55:09 +0300

    CC API and test

---
+++ api_wrappers/crypto_compare_api.py
@@ -0,0 +1,18 @@
+import json
+import requests as req
+import datetime
+import pandas as pd
+
+
+class cc(object):
+
+    api_key = None
+    
+    def __init__(self, api_key, testing = False) -> None:
+        self.api_key = api_key
+    
+    def get_all_btc_daily(self):
+        data = req.get("https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=USD&allData=true").json()['Data']['Data']
+        df = pd.DataFrame(data)
+        df['timestamp'] = [datetime.datetime.fromtimestamp(d) for d in df.time]
+        return df
--- btcToolkit.py
+++ btcToolkit.py
@@ -1,17 +1,34 @@
 import argparse
+import yaml
+from yaml.loader import SafeLoader
+from api_wrappers import crypto_compare_api as cc_api
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def main():
 
     # Arguments
     parser = argparse.ArgumentParser(description = 'This is a BTC toolkit')
-    parser.add_argument('-k', '--API_KEY_CoinMarketCap', action="store", required=True)
+    parser.add_argument('-k', '--api_keys_file', action="store", default="api_keys.yaml")
     parser.add_argument('-t', '--testing', action='store_true', default=False, help='Activate testing mode')
     args = parser.parse_args()
 
-    # API CMC
-    import cmc_api
-    cmc = cmc_api.cmc(args.API_KEY_CoinMarketCap, testing = args.testing)
-    print(cmc.listing_rank(limit=1))
+    # Read API keys
+    api_keys = yaml.load(open(args.api_keys_file), Loader=SafeLoader)
+
+    # Create crypto compare client
+    cc = cc_api.cc(api_keys["cryptocompare"])
+
+
+    # Testing to represent all BTC data in log scale
+    df = cc.get_all_btc_daily()
+    plt.style.use('fivethirtyeight')
+    plt.plot(df.timestamp, df.close)
+    plt.xticks(rotation=45)
+    plt.yscale("log")
+    plt.show()
+
+    
 
 if __name__ == "__main__":
     main()
